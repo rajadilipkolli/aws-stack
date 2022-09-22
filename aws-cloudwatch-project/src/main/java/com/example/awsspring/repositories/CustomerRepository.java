68142bc5b45cbe470ea16c0b5ed053295cd1700f
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-22 04:33:00 +0000

    Initial Commit

---
+++ aws-cloudwatch-project/src/main/java/com/example/awsspring/repositories/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.example.awsspring.repositories;
+
+import com.example.awsspring.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}

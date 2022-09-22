f0ae1f48be6f3953c7005428714cb099362ecdc6
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-22 14:25:33 +0530

    initial setup of sonarqube (#277)

---
+++ aws-s3-project/src/test/java/com/learning/awspring/repository/FileInfoRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.learning.awspring.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.learning.awspring.domain.FileInfo;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class FileInfoRepositoryTest {
+
+    @Autowired private TestEntityManager entityManager;
+
+    @Autowired private FileInfoRepository fileInfoRepository;
+
+    @Test
+    public void testFindByFileName() {
+        FileInfo fileInfo = new FileInfo();
+        fileInfo.setFileName("test");
+        fileInfo.setFileUrl("testUrl");
+        entityManager.persist(fileInfo);
+        entityManager.flush();
+        List<FileInfo> fileInfoList = fileInfoRepository.findByFileName("test");
+        assertEquals(1, fileInfoList.size());
+    }
+
+    @Test
+    public void testExistsByFileName() {
+        FileInfo fileInfo = new FileInfo();
+        fileInfo.setFileName("test");
+        fileInfo.setFileUrl("testUrl");
+        entityManager.persist(fileInfo);
+        entityManager.flush();
+        boolean exists = fileInfoRepository.existsByFileName("test");
+        assertTrue(exists);
+    }
+}

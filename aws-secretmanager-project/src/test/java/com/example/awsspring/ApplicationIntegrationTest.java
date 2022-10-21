a0c1e40f0d8b8090b86f4dd9e8fa14139fe078bd
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-21 03:15:45 +0000

    initial project setup of aws-secretmanager

---
+++ aws-secretmanager-project/src/test/java/com/example/awsspring/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.awsspring;
+
+import com.example.awsspring.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}

f30c09f0f09ab2d5883566849970765c778ceafc
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-23 06:48:12 +0000

    initial setup of secretmanager

--- aws-secretmanager-project/.localstack/01_init.sh
+++ aws-secretmanager-project/.localstack/01_init.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-awslocal s3 mb s3://testbucket
-echo "List of S3 buckets:"
+awslocal secretsmanager create-secret --name /spring/secret --secret-string file://myCreds.json --region us-east-1
+echo "List of secretsmanager secrets:"
 echo "-------------------------------"
-awslocal s3 ls
-
-awslocal sqs create-queue --queue-name test_queue
-echo "List of SQS Queues:"
-echo "-------------------------------"
-awslocal sqs list-queues
+awslocal secretsmanager list-secrets

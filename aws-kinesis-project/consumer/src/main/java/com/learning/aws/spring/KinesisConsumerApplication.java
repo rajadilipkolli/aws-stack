968219258e99521261b170d21f4e98073290c6c0
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-30 19:56:00 +0000

    Upgrade to spring boot 3.0.0-RC1

--- aws-kinesis-project/consumer/src/main/java/com/learning/aws/spring/KinesisConsumerApplication.java
+++ aws-kinesis-project/consumer/src/main/java/com/learning/aws/spring/KinesisConsumerApplication.java
@@ -1,12 +1,13 @@
 package com.learning.aws.spring;
 
 import com.learning.aws.spring.config.ApplicationProperties;
+import io.awspring.cloud.autoconfigure.context.properties.AwsCredentialsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ApplicationProperties.class})
+@EnableConfigurationProperties({ApplicationProperties.class, AwsCredentialsProperties.class})
 public class KinesisConsumerApplication {
 
     public static void main(String[] args) {

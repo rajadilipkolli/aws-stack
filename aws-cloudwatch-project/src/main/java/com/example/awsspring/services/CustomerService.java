fd44675cc7213cd1e40ac790a0823a268f5f4f03
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-29 15:07:34 +0000

    fixes spotless issue

--- aws-cloudwatch-project/src/main/java/com/example/awsspring/services/CustomerService.java
+++ aws-cloudwatch-project/src/main/java/com/example/awsspring/services/CustomerService.java
@@ -5,7 +5,6 @@ import com.example.awsspring.repositories.CustomerRepository;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

b8f885c779ece8ad70d13d1cc8f3b08b8bbe890a
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-09 03:33:40 +0000

    adds Junit to test producer

--- aws-kinesis-project/consumer/src/test/java/com/learning/aws/spring/ApplicationIntegrationTest.java
+++ aws-kinesis-project/consumer/src/test/java/com/learning/aws/spring/ApplicationIntegrationTest.java
@@ -6,5 +6,7 @@ import org.junit.jupiter.api.Test;
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        
+    }
 }

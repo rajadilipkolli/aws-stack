b506255688ae2b8f364372fda430701ea930b8ac
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-15 14:26:29 +0000

    fixes issue with starting testcontainer in dev mode

--- aws-s3-project/src/test/java/com/learning/awspring/TestS3Application.java
+++ aws-s3-project/src/test/java/com/learning/awspring/TestS3Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -19,26 +18,25 @@ public class TestS3Application {
     @ServiceConnection
     @RestartScope
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>("postgres:15.3-alpine");
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.3-alpine"));
     }
 
-    @Bean
-    public LocalStackContainer localStackContainer(DynamicPropertyRegistry propertyRegistry) {
-        LocalStackContainer localStackContainer =
-                new LocalStackContainer(
-                                DockerImageName.parse("localstack/localstack").withTag("2.1.0"))
-                        .withCopyFileToContainer(
-                                MountableFile.forHostPath(".localstack/"),
-                                "/etc/localstack/init/ready.d/")
-                        .waitingFor(
-                                Wait.forLogMessage(".*LocalStack initialized successfully\n", 1));
-        propertyRegistry.add("spring.cloud.aws.endpoint", localStackContainer::getEndpoint);
-        propertyRegistry.add("spring.cloud.aws.region.static", localStackContainer::getRegion);
-        propertyRegistry.add(
-                "spring.cloud.aws.credentials.access-key", localStackContainer::getAccessKey);
-        propertyRegistry.add(
-                "spring.cloud.aws.credentials.secret-key", localStackContainer::getSecretKey);
-        return localStackContainer;
+    static final LocalStackContainer LOCALSTACKCONTAINER =
+            new LocalStackContainer(DockerImageName.parse("localstack/localstack").withTag("2.1.0"))
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath(".localstack/"),
+                            "/etc/localstack/init/ready.d/")
+                    .waitingFor(Wait.forLogMessage(".*LocalStack initialized successfully\n", 1));
+
+    static {
+        LOCALSTACKCONTAINER.start();
+        System.setProperty(
+                "spring.cloud.aws.endpoint", LOCALSTACKCONTAINER.getEndpoint().toString());
+        System.setProperty(
+                "spring.cloud.aws.credentials.access-key", LOCALSTACKCONTAINER.getAccessKey());
+        System.setProperty(
+                "spring.cloud.aws.credentials.secret-key", LOCALSTACKCONTAINER.getSecretKey());
+        System.setProperty("spring.cloud.aws.region.static", LOCALSTACKCONTAINER.getRegion());
     }
 
     public static void main(String[] args) {
--- aws-s3-project/src/test/java/com/learning/awspring/common/AbstractIntegrationTest.java
+++ aws-s3-project/src/test/java/com/learning/awspring/common/AbstractIntegrationTest.java
@@ -8,14 +8,12 @@ import com.learning.awspring.TestS3Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles({PROFILE_TEST})
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = TestS3Application.class)
 @AutoConfigureMockMvc
-@Import(TestS3Application.class)
 public abstract class AbstractIntegrationTest {
 
     @Autowired protected MockMvc mockMvc;

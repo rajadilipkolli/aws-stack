9ee04c5c2e78e93b14e4647554c00dd53c9cc453
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-19 23:19:24 +0000

    chore : clean code

--- aws-dynamodb-project/src/main/java/com/learning/awsspring/config/Initializer.java
+++ aws-dynamodb-project/src/main/java/com/learning/awsspring/config/Initializer.java
@@ -1,20 +1,16 @@
 package com.learning.awsspring.config;
 
-import static com.learning.awsspring.utils.AppConstants.PROFILE_NOT_TEST;
-
 import com.learning.awsspring.entities.Customer;
 import com.learning.awsspring.repositories.CustomerRepository;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@Profile({PROFILE_NOT_TEST})
 public class Initializer implements CommandLineRunner {
 
     private final CustomerRepository customerRepository;

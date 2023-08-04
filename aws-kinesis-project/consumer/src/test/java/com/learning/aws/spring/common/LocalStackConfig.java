bc42691fb8aae9e6a400e38b1841d1c1af49b437
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-04 05:35:41 +0000

    using latest version to bind automatically

--- aws-kinesis-project/consumer/src/test/java/com/learning/aws/spring/common/LocalStackConfig.java
+++ aws-kinesis-project/consumer/src/test/java/com/learning/aws/spring/common/LocalStackConfig.java
@@ -7,27 +7,34 @@ import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
-import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
 
 @TestConfiguration(proxyBeanMethods = false)
 @Slf4j
 public class LocalStackConfig {
-    static final LocalStackContainer LOCAL_STACK_CONTAINER =
-            new LocalStackContainer(
-                    DockerImageName.parse("localstack/localstack").withTag("2.1.0"));
+
+    @Container
+    static LocalStackContainer localStackContainer =
+            new LocalStackContainer(DockerImageName.parse("localstack/localstack:2.2.0"));
 
     static {
-        LOCAL_STACK_CONTAINER.start();
+        localStackContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.cloud.aws.endpoint", localStackContainer::getEndpoint);
+        dynamicPropertyRegistry.add(
+                "spring.cloud.aws.region.static", localStackContainer::getRegion);
+        dynamicPropertyRegistry.add(
+                "spring.cloud.aws.credentials.access-key", localStackContainer::getAccessKey);
+        dynamicPropertyRegistry.add(
+                "spring.cloud.aws.credentials.secret-key", localStackContainer::getSecretKey);
     }
 
     @Bean
@@ -38,43 +45,5 @@ public class LocalStackConfig {
                         .mapToObj(ipSuffix -> new IpAddressDTO("192.168.0." + ipSuffix))
                         .peek(entry -> log.info("sending event {}", entry))
                         .toList();
-    }
-
-    @Bean
-    @Primary
-    public KinesisAsyncClient amazonKinesis() {
-        return applyAwsClientOptions(
-                KinesisAsyncClient.builder(), LocalStackContainer.Service.KINESIS);
-    }
-
-    @Bean
-    @Primary
-    public DynamoDbAsyncClient amazonDynamoDBAsync() {
-        return applyAwsClientOptions(
-                DynamoDbAsyncClient.builder(), LocalStackContainer.Service.DYNAMODB);
-    }
-
-    @Bean
-    @Primary
-    public CloudWatchAsyncClient cloudWatchClient() {
-        return applyAwsClientOptions(
-                CloudWatchAsyncClient.builder(), LocalStackContainer.Service.CLOUDWATCH);
-    }
-
-    private static StaticCredentialsProvider getAwsCredentialsProvider() {
-        return StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(
-                        LOCAL_STACK_CONTAINER.getAccessKey(),
-                        LOCAL_STACK_CONTAINER.getSecretKey()));
-    }
-
-    private static <B extends AwsClientBuilder<B, T>, T> T applyAwsClientOptions(
-            B clientBuilder, LocalStackContainer.Service service) {
-
-        return clientBuilder
-                .region(Region.of(LOCAL_STACK_CONTAINER.getRegion()))
-                .credentialsProvider(getAwsCredentialsProvider())
-                .endpointOverride(LOCAL_STACK_CONTAINER.getEndpointOverride(service))
-                .build();
     }
 }

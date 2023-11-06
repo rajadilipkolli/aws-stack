f80d990153858cc261b223bc4b5904ed71f9972c
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-06 05:41:23 +0000

    fix : remove unused import

--- aws-sqs-project/src/main/java/com/learning/awspring/web/listener/SQSListener.java
+++ aws-sqs-project/src/main/java/com/learning/awspring/web/listener/SQSListener.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;

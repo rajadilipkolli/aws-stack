1a4ce93e0ac32a2b7ab8a99b163dac152d54aec7
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-06 05:22:44 +0000

    fix : add correct Integrationtest

--- aws-ses-project/src/test/java/com/example/awsspring/common/LocalStackConfig.java
+++ aws-ses-project/src/test/java/com/example/awsspring/common/LocalStackConfig.java
@@ -1,7 +1,5 @@
 package com.example.awsspring.common;
 
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SES;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -31,6 +29,6 @@ public class LocalStackConfig {
         dynamicPropertyRegistry.add(
                 "spring.cloud.aws.region.static", localStackContainer::getRegion);
         dynamicPropertyRegistry.add(
-                "spring.cloud.aws.endpoint", () -> localStackContainer.getEndpointOverride(SES));
+                "spring.cloud.aws.endpoint", () -> localStackContainer.getEndpoint().toString());
     }
 }

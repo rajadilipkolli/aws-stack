f8fb655d972ce552f8a51927899416668e37166f
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-17 05:11:41 +0000

    feat: upgrade testcontainers to 3.0.0

--- aws-dynamodb-project/src/test/java/com/learning/awsspring/common/LocalStackConfig.java
+++ aws-dynamodb-project/src/test/java/com/learning/awsspring/common/LocalStackConfig.java
@@ -12,7 +12,7 @@ import org.testcontainers.utility.MountableFile;
 @Slf4j
 public class LocalStackConfig {
     protected static final LocalStackContainer LOCAL_STACK_CONTAINER =
-            new LocalStackContainer(DockerImageName.parse("localstack/localstack").withTag("2.3.2"))
+            new LocalStackContainer(DockerImageName.parse("localstack/localstack").withTag("3.0.0"))
                     .withCopyFileToContainer(
                             MountableFile.forHostPath(".localstack/"),
                             "/etc/localstack/init/ready.d/")

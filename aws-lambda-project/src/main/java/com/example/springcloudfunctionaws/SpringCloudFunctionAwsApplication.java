a1bc1e3dd7bd09a1bfbd50ef1d073639a9877669
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-22 12:52:00 +0000

    polish

--- aws-lambda-project/src/main/java/com/example/springcloudfunctionaws/SpringCloudFunctionAwsApplication.java
+++ aws-lambda-project/src/main/java/com/example/springcloudfunctionaws/SpringCloudFunctionAwsApplication.java
@@ -15,12 +15,12 @@ public class SpringCloudFunctionAwsApplication {
 	}
 
 	@Bean
-	public Function<Profile, Integer> fetchByName(JdbcTemplate jdbcTemplate) {
+	Function<Profile, Integer> fetchByName(JdbcTemplate jdbcTemplate) {
 		return profile -> jdbcTemplate.queryForObject("SELECT COUNT(*) FROM profile WHERE name LIKE ?", Integer.class,
 				profile.name() + "%");
 	}
 
-	record Profile(String name) {
+	public record Profile(String name) {
 
 	}
 

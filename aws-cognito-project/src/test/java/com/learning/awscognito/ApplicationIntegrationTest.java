29cb19ffd3c9243a76a38f4a70e5a54c11b4487b
Raja Kolli <rajadilipkolli@gmail.com>
2023-12-26 17:12:17 +0000

    feat: initial setup of aws cognito

---
+++ aws-cognito-project/src/test/java/com/learning/awscognito/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.learning.awscognito;
+
+import com.learning.awscognito.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ aws-cognito-project/src/test/java/com/learning/awscognito/common/ContainersConfig.java
@@ -0,0 +1,22 @@
+package com.learning.awscognito.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ContainersConfig {
+
+    @Bean
+    LocalStackContainer localstackContainer(DynamicPropertyRegistry registry) {
+        LocalStackContainer localStackContainer =
+                new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.0.2"));
+        registry.add("spring.cloud.aws.credentials.access-key", localStackContainer::getAccessKey);
+        registry.add("spring.cloud.aws.credentials.secret-key", localStackContainer::getSecretKey);
+        registry.add("spring.cloud.aws.region.static", localStackContainer::getRegion);
+        registry.add("spring.cloud.aws.endpoint", localStackContainer::getEndpoint);
+        return localStackContainer;
+    }
+}

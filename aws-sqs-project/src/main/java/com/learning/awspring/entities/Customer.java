af3006f54e3f970798c91feb70bbd8dd4ca5c43c
Raja Kolli <rajadilipkolli@gmail.com>
2023-12-26 22:15:48 +0000

    polish: schema and testcontainers config

--- aws-sqs-project/src/main/java/com/learning/awspring/entities/Customer.java
+++ aws-sqs-project/src/main/java/com/learning/awspring/entities/Customer.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,8 +20,7 @@ import lombok.Setter;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_generator")
-    @SequenceGenerator(name = "customer_id_generator", sequenceName = "customer_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false)
--- aws-sqs-project/src/test/java/com/learning/awspring/config/TestcontainersConfig.java
+++ aws-sqs-project/src/test/java/com/learning/awspring/config/TestcontainersConfig.java
@@ -3,6 +3,7 @@ package com.learning.awspring.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -16,18 +17,15 @@ public class TestcontainersConfig {
         return new PostgreSQLContainer<>("postgres:16.1-alpine");
     }
 
-    static final LocalStackContainer localStackContainer =
-            new LocalStackContainer(
-                    DockerImageName.parse("localstack/localstack").withTag("3.0.2"));
-
-    static {
-        localStackContainer.start();
-        System.setProperty(
-                "spring.cloud.aws.endpoint", localStackContainer.getEndpoint().toString());
-        System.setProperty(
-                "spring.cloud.aws.credentials.access-key", localStackContainer.getAccessKey());
-        System.setProperty(
-                "spring.cloud.aws.credentials.secret-key", localStackContainer.getSecretKey());
-        System.setProperty("spring.cloud.aws.region.static", localStackContainer.getRegion());
+    @Bean
+    LocalStackContainer localstackContainer(DynamicPropertyRegistry registry) {
+        LocalStackContainer localStackContainer =
+                new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.0.2"))
+                        .withReuse(true);
+        registry.add("spring.cloud.aws.credentials.access-key", localStackContainer::getAccessKey);
+        registry.add("spring.cloud.aws.credentials.secret-key", localStackContainer::getSecretKey);
+        registry.add("spring.cloud.aws.region.static", localStackContainer::getRegion);
+        registry.add("spring.cloud.aws.endpoint", localStackContainer::getEndpoint);
+        return localStackContainer;
     }
 }

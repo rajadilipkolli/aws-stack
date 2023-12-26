e0ec04ca886c2d5d76691597a326d9c88902a649
Raja Kolli <rajadilipkolli@gmail.com>
2023-12-26 23:43:37 +0000

    feat: adds more integration tests

--- aws-s3-project/src/test/java/com/learning/awspring/TestS3Application.java
+++ aws-s3-project/src/test/java/com/learning/awspring/TestS3Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -18,25 +19,26 @@ public class TestS3Application {
     @ServiceConnection
     @RestartScope
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.1-alpine"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.1-alpine"))
+                .withReuse(true);
     }
 
-    static final LocalStackContainer LOCALSTACKCONTAINER =
-            new LocalStackContainer(DockerImageName.parse("localstack/localstack").withTag("3.0.2"))
-                    .withCopyFileToContainer(
-                            MountableFile.forHostPath(".localstack/"),
-                            "/etc/localstack/init/ready.d/")
-                    .waitingFor(Wait.forLogMessage(".*LocalStack initialized successfully\n", 1));
-
-    static {
-        LOCALSTACKCONTAINER.start();
-        System.setProperty(
-                "spring.cloud.aws.endpoint", LOCALSTACKCONTAINER.getEndpoint().toString());
-        System.setProperty(
-                "spring.cloud.aws.credentials.access-key", LOCALSTACKCONTAINER.getAccessKey());
-        System.setProperty(
-                "spring.cloud.aws.credentials.secret-key", LOCALSTACKCONTAINER.getSecretKey());
-        System.setProperty("spring.cloud.aws.region.static", LOCALSTACKCONTAINER.getRegion());
+    @Bean
+    LocalStackContainer localstackContainer(DynamicPropertyRegistry registry) {
+        LocalStackContainer localStackContainer =
+                new LocalStackContainer(
+                                DockerImageName.parse("localstack/localstack").withTag("3.0.2"))
+                        .withCopyFileToContainer(
+                                MountableFile.forHostPath(".localstack/"),
+                                "/etc/localstack/init/ready.d/")
+                        .waitingFor(
+                                Wait.forLogMessage(".*LocalStack initialized successfully\n", 1))
+                        .withReuse(true);
+        registry.add("spring.cloud.aws.credentials.access-key", localStackContainer::getAccessKey);
+        registry.add("spring.cloud.aws.credentials.secret-key", localStackContainer::getSecretKey);
+        registry.add("spring.cloud.aws.region.static", localStackContainer::getRegion);
+        registry.add("spring.cloud.aws.endpoint", localStackContainer::getEndpoint);
+        return localStackContainer;
     }
 
     public static void main(String[] args) {
--- aws-s3-project/src/test/java/com/learning/awspring/controller/FileInfoControllerIT.java
+++ aws-s3-project/src/test/java/com/learning/awspring/controller/FileInfoControllerIT.java
@@ -1,8 +1,11 @@
 package com.learning.awspring.controller;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,16 +13,76 @@ import com.learning.awspring.common.AbstractIntegrationTest;
 import com.learning.awspring.model.SignedUploadRequest;
 import io.awspring.cloud.s3.S3Template;
 import java.util.Map;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FileInfoControllerIT extends AbstractIntegrationTest {
 
     @Autowired private S3Template s3Template;
 
     @Test
+    @Order(1)
+    void uploadFileToS3() throws Exception {
+        MockMultipartFile file =
+                new MockMultipartFile(
+                        "file", "test.txt", MediaType.TEXT_PLAIN_VALUE, "Hello, World!".getBytes());
+
+        // Perform the request and assert the response
+        mockMvc.perform(multipart("/s3/upload").file(file))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.fileName").value("test.txt"))
+                .andExpect(jsonPath("$.fileUrl").value(containsString("/testbucket/test.txt")));
+    }
+
+    @Test
+    @Disabled
+    @Order(2)
+    void downloadFromS3RouteTest() throws Exception {
+        String fileName = "test.txt";
+
+        // Perform the MockMvc request
+        mockMvc.perform(get("/s3/download/{name}", fileName))
+                .andExpect(status().isOk())
+                .andExpect(
+                        header().string(
+                                        HttpHeaders.CONTENT_DISPOSITION,
+                                        "attachment; filename=" + fileName))
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM));
+    }
+
+    @Test
+    @Order(3)
+    void viewAllFromS3Route() throws Exception {
+
+        // Perform the request and assert the response
+        mockMvc.perform(get("/s3/view-all"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0]").value("junit.txt"))
+                .andExpect(jsonPath("$[1]").value("test.txt"));
+    }
+
+    @Test
+    @Order(4)
+    void viewAllFilesFromDb() throws Exception {
+        // Perform the request and assert the response
+        mockMvc.perform(get("/s3/view-all-db"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].fileName").value("test.txt"))
+                .andExpect(jsonPath("$[0].fileUrl").value(containsString("/testbucket/test.txt")));
+    }
+
+    @Test
+    @Order(5)
     void downloadFileUsingSignedURL() throws Exception {
         // setUp data
         this.s3Template.store("testbucket", "junit.txt", "Hyderabad");
@@ -35,6 +98,7 @@ class FileInfoControllerIT extends AbstractIntegrationTest {
     }
 
     @Test
+    @Order(6)
     void uploadFileUsingSignedURL() throws Exception {
 
         SignedUploadRequest signedUploadRequest =

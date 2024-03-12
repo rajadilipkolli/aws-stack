e1f2dfb6b9b6706c03bb0d38d1d426ba095dc2c6
Raja Kolli <rajadilipkolli@gmail.com>
2024-03-12 08:11:54 +0000

    adds kinesis asyncClient Bean

--- aws-kinesis-project/producer/src/test/java/com/learning/aws/spring/common/LocalStackConfig.java
+++ aws-kinesis-project/producer/src/test/java/com/learning/aws/spring/common/LocalStackConfig.java
@@ -8,9 +8,14 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.messaging.Message;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class LocalStackConfig {
@@ -25,13 +30,20 @@ public class LocalStackConfig {
                         .withServices(KINESIS)
                         .withExposedPorts(4566);
         localStackContainer.start();
-        System.setProperty(
-                "spring.cloud.aws.endpoint", localStackContainer.getEndpoint().toString());
-        System.setProperty(
-                "spring.cloud.aws.credentials.access-key", localStackContainer.getAccessKey());
-        System.setProperty(
-                "spring.cloud.aws.credentials.secret-key", localStackContainer.getSecretKey());
-        System.setProperty("spring.cloud.aws.region.static", localStackContainer.getRegion());
+    }
+
+    @Bean
+    @Primary
+    public KinesisAsyncClient amazonKinesis() {
+        return KinesisAsyncClient.builder()
+                .endpointOverride(localStackContainer.getEndpointOverride(KINESIS))
+                .region(Region.of(localStackContainer.getRegion()))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(
+                                        localStackContainer.getAccessKey(),
+                                        localStackContainer.getSecretKey())))
+                .build();
     }
 
     @Bean

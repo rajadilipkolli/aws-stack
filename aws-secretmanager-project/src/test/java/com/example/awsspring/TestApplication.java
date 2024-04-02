ad873ee233147ba9b23a8b10d90267a3b74e92c8
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-02 11:04:43 +0000

    prepare for upgrade

---
+++ aws-secretmanager-project/src/test/java/com/example/awsspring/TestApplication.java
@@ -0,0 +1,31 @@
+package com.example.awsspring;
+
+import com.example.awsspring.common.DBTestContainer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+@TestConfiguration(proxyBeanMethods = false)
+@ImportTestcontainers(DBTestContainer.class)
+public class TestApplication {
+
+    @Bean
+    @ServiceConnection
+    LocalStackContainer localStackContainer() {
+        return new LocalStackContainer(
+                        DockerImageName.parse("localstack/localstack").withTag("3.3.0"))
+                .withCopyFileToContainer(
+                        MountableFile.forHostPath("localstack/"), "/etc/localstack/init/ready.d/")
+                .waitingFor(Wait.forLogMessage(".*LocalStack initialized successfully\n", 1));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+    }
+}

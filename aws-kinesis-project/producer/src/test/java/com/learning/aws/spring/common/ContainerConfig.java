def11254a162e0bdbe31aaaadfbb53a6eb33741e
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-16 16:41:37 +0000

    implement code review comments

--- aws-kinesis-project/producer/src/test/java/com/learning/aws/spring/common/ContainerConfig.java
+++ aws-kinesis-project/producer/src/test/java/com/learning/aws/spring/common/ContainerConfig.java
@@ -2,6 +2,7 @@ package com.learning.aws.spring.common;
 
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.KINESIS;
 
+import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistrar;
@@ -9,6 +10,8 @@ import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
 
@@ -22,7 +25,8 @@ public class ContainerConfig {
     @Bean
     LocalStackContainer localStackContainer() {
         return new LocalStackContainer(
-                DockerImageName.parse("localstack/localstack").withTag("4.0.3"));
+                        DockerImageName.parse("localstack/localstack").withTag("4.0.3"))
+                .withServices(KINESIS);
     }
 
     @Bean
@@ -49,6 +53,11 @@ public class ContainerConfig {
                                 AwsBasicCredentials.create(
                                         localStackContainer.getAccessKey(),
                                         localStackContainer.getSecretKey())))
+                .overrideConfiguration(
+                        ClientOverrideConfiguration.builder()
+                                .apiCallTimeout(Duration.ofSeconds(10))
+                                .retryPolicy(RetryPolicy.builder().numRetries(3).build())
+                                .build())
                 .build();
     }
 }

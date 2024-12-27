f1423837c7f5b60a382de5682a7e28b66a1f6e8f
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-27 07:22:00 +0000

    adds to string

--- aws-dynamodb-project/src/main/java/com/learning/awsspring/entities/Customer.java
+++ aws-dynamodb-project/src/main/java/com/learning/awsspring/entities/Customer.java
@@ -55,4 +55,9 @@ public class Customer {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
+    }
 }

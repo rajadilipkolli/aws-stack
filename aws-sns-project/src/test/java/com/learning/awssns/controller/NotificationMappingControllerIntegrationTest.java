597c56271f5aab27298aea9d06cbf905ac2b73f6
Raja Kolli <rajadilipkolli@gmail.com>
2025-03-13 18:20:08 +0000

    adds more tests

--- aws-sns-project/src/test/java/com/learning/awssns/controller/NotificationMappingControllerIntegrationTest.java
+++ aws-sns-project/src/test/java/com/learning/awssns/controller/NotificationMappingControllerIntegrationTest.java
@@ -38,4 +38,42 @@ class NotificationMappingControllerIntegrationTest extends AbstractIntegrationTe
         assertThat(this.controller.getMessage()).isEqualTo("testMessage");
         assertThat(this.controller.getSubject()).isEqualTo("hello");
     }
+
+    @Test
+    void handleNotificationMessage_whenMissingSubjectAndMessage_shouldNotStoreValues() throws Exception {
+        // Arrange
+        String jsonWithoutSubjectAndMessage =
+                """
+                {
+                    "Type": "Notification",
+                    "MessageId": "f2c15fec-c617-5b08-b54d-13c4099fec60",
+                    "TopicArn": "arn:aws:sns:us-east-1:111111111111:testTopic",
+                    "Timestamp": "2025-03-28T14:12:24.418Z"
+                }""";
+
+        // Act
+        mockMvc.perform(post("/testTopic")
+                        .header("x-amz-sns-message-type", "Notification")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonWithoutSubjectAndMessage))
+                .andExpect(status().isNoContent());
+
+        // Assert
+        assertThat(this.controller.getMessage()).isEmpty();
+        assertThat(this.controller.getSubject()).isEmpty();
+    }
+
+    @Test
+    void handleNotificationMessage_whenMissingHeader_shouldReturnServerError() throws Exception {
+        // Arrange
+        byte[] notificationJsonContent = FileCopyUtils.copyToByteArray(Optional.ofNullable(
+                        getClass().getClassLoader().getResourceAsStream("notificationMessage.json"))
+                .orElseThrow(() -> new IllegalArgumentException("notificationMessage.json not found in classpath")));
+
+        // Act & Assert
+        mockMvc.perform(post("/testTopic")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(notificationJsonContent))
+                .andExpect(status().is5xxServerError());
+    }
 }
